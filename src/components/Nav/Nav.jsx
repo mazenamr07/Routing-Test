60bbfd04eadda17feb085a2e668ef4f05b5fd39e
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2025-01-07 17:19:52 +0200

    Done Navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import './Nav.css'
+import "./Nav.css";
 
 export default function Nav() {
   return (
     <>
       <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+        <div class="container py-3">
+          <a class="navbar-brand text-uppercase" href="#">
             Start Framework
           </a>
           <button
@@ -22,18 +22,18 @@ export default function Nav() {
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">
+              <li class="nav-item text-uppercase mx-1">
+                <a class="nav-link px-3 active" aria-current="page" href="#">
                   About
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class="nav-item text-uppercase mx-1">
+                <a class="nav-link px-3" href="#">
                   Portfolio
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class="nav-item text-uppercase mx-1">
+                <a class="nav-link px-3" href="#">
                   Contact
                 </a>
               </li>
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,4 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
